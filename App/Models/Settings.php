ae5fd0c17db8440bf355262cda302d52f74fed74
NewLukasz <KilijasnkiLukasz@gmail.com>
2021-02-25 10:03:32 +0100

    Add functions for adding limit

--- App/Models/Settings.php
+++ App/Models/Settings.php
@@ -44,26 +44,34 @@ class Settings extends \Core\Model{
         
         $userId=$_SESSION['user_id'];
         $newName=$_POST['newName'];
-
+        
         $nameToChange=$_POST['categoryOrPaymentMethodName'];
-        echo $nameToChange;
         if($_POST['whatToChange']=="incomeCategory"){
             $tableWithCategories=static::getUserTableWithIncomesCategory();
-            $id=FinancialMovement::getCategoryOrMethodIdByName($nameToChange, $tableWithCategories);
-            $sql="UPDATE ".$tableWithCategories." SET name=:name WHERE id=:id ";
         }elseif($_POST['whatToChange']=='paymentMethod'){
             $tableWithCategories=static::getUserTableWithPaymentMethods();
-            $id=FinancialMovement::getCategoryOrMethodIdByName($nameToChange, $tableWithCategories);
-            $sql="UPDATE ".$tableWithCategories." SET name=:name WHERE id=:id ";
         }elseif($_POST['whatToChange']=="expenseCategory"){
             $tableWithCategories=static::getUserTableWithExpensesCategory();
+            if(isset($_POST['limit'])){
+                static::addLimit(FinancialMovement::getCategoryOrMethodIdByName($nameToChange, $tableWithCategories),$_POST['limit'],$tableWithCategories);
+            }
+        }
+        if(!empty($_POST['newName'])){
             $id=FinancialMovement::getCategoryOrMethodIdByName($nameToChange, $tableWithCategories);
             $sql="UPDATE ".$tableWithCategories." SET name=:name WHERE id=:id ";
+            $db=static::getDB();
+            $stmt=$db->prepare($sql);
+            $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+            $stmt->bindValue(':name', $newName, PDO::PARAM_STR);
+            $stmt->execute();
         }
+    }
+    protected static function addLimit($id,$limit,$tableWithCategories){
+        $sql="UPDATE ".$tableWithCategories." SET expense_limit=:expense_limit  WHERE id=:id ";
         $db=static::getDB();
         $stmt=$db->prepare($sql);
-        $stmt->bindValue(':name', $newName, PDO::PARAM_STR);
         $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        $stmt->bindValue(':expense_limit', $limit, PDO::PARAM_INT);
         $stmt->execute();
     }
 }
